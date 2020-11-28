3359f08c4b1e11c3aabdacbbbf2e919aea18f327
LinYuMingTaipei <a828215362@gmail.com>
2020-11-28 16:37:34 +0800

    refactor user api

--- tripadvisor/api/user/endpoints.py
+++ tripadvisor/api/user/endpoints.py
@@ -1,76 +1,234 @@
 from flask import request, jsonify
 from flask_login import login_required, current_user
 
-from tripadvisor import dao
+from tripadvisor import tasks
 from tripadvisor.api.user import user, services
+from tripadvisor.models import User, Comment
+
+import logging
+from urllib.parse import unquote 
+
+logger = logging.getLogger()
 
 
-@user.route('/restaurant/like/<restaurant>')
-def like(restaurant):
-    submit = services.submit_like(current_user, restaurant, request.args)
-    result = dao.save(submit)
-    return jsonify(result)
+@user.route('/restaurant/like', methods = ['POST'])
+def like():
+    """餐廳收藏功能"""
+    res = {'status': False}
+    try:
+        data = request.form
+        tasks.follow_restaurant(current_user, data['restaurant'])
+        return jsonify(res)
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route('/restaurant/unlike/<restaurant>')
-def unlike(restaurant):
-    submit = services.submit_unlike(current_user, restaurant)
-    result = dao.delete()
-    return jsonify(result)
+@user.route('/restaurant/unlike', methods = ['POST'])
+def unlike():
+    """取消餐廳收藏功能"""
+    res = {'status': False}
+    try:
+        data = request.form
+        tasks.unfollow_restaurant(current_user.id, data['restaurant'])
+        res.update({'status': True})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route("/about_me/<username>",methods=["POST"])
+@user.route("/about_me/<username>", methods=['POST'])
 @login_required
 def about_me(username):
-    submit = services.submit_about_me(username, request.form)
-    result = dao.save(submit)
-    return jsonify(result)
+    """更改個人資訊功能"""
+    res = {'status': False}
+    try:
+        data = request.form
+        submit = services.update_aboutMe(username, data)
+        res.update({'status': True})
+        return jsonify(res)
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route("/comment/like/<id>")
-def comment_like(id):
-    submit = services.submit_comment_like(id, current_user)
-    result = dao.save(submit)
-    return result
+@user.route('/comment/like')
+def comment_like():
+    """留言按讚功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        submit = tasks.like_comment(data['id'], current_user)
+        res.update({'status': True})
+        return res
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route("/comment/unlike/<int:id>")
-def comment_unlike(id):
-    services.submit_comment_unlike(id, current_user)
-    result = dao.delete()
-    return jsonify(result)
+@user.route('/comment/unlike')
+def comment_unlike():
+    """留言收回讚功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        comment = Comment().find_by_id(data['id'])
+        tasks.unlike_comment(current_user.id, comment)
+        res.update({'status': True})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route('/comment/edit/<int:id>',methods=["POST"])
+@user.route('/comment/edit/<int:id>', methods=["POST"])
 def review_edit(id):
-    submit = services.submit_comments(id, current_user, request.form)
-    result = dao.save(submit)
-    return jsonify(result)
+    """評論餐廳功能"""
+    res = {'status': False}
+    try:
+        services.submit_comments(id, current_user, request.form)
+        res.update({'status': True})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route('/follow/<username>')
+@user.route('/follow')
 @login_required
-def follow(username):
-    result = services.follow(username, current_user)
-    return jsonify(result)
+def follow():
+    """用戶追蹤功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        user = User().find_by_username(data['username'])
+        if user is None:
+            res['msg'] = '查無該用戶'
+            return jsonify(res)
+
+        if current_user.is_following(user):
+            res['msg'] = '您已關注該用戶'
+            return jsonify(res)
+        else:
+            current_user.follow(user)
+            res.update({'status': True, 'msg': u"您已成功關注%s" % data['username']})
+            return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
     
 
-@user.route('/unfollow/<username>')
+@user.route('/unfollow')
 @login_required
-def unfollow(username):
-    result = services.unfollow(username, current_user)
-    return jsonify(result)
+def unfollow():
+    """取消追蹤功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        user = User().find_by_username(data['username'])
+        if user is None:
+            res['status'] = '查無該用戶'
+            return jsonify(res)
+        else:
+            current_user.unfollow(user)
+            res.update({'status': True, 'msg': u'您已成功取消對%s的關注' % data['username']})
+            return jsonify(res)
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route("/followers/<username>")
+@user.route('/followers')
 @login_required
-def follow_user(username):
-    followers = services.following_user(username)
-    return jsonify(status = "success", followers=followers)
+def followers():
+    """粉絲名單查詢功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        followers = services.get_followers(data['username'])
+        res.update({'status': True, 'data': followers})
+        return jsonify(res)
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
-@user.route("/followed/<username>")
+@user.route('/followed')
 @login_required
-def followed_user(username):
-    followed = services.followed_user(username)
-    return jsonify(status = "success", followed=followed)
+def followed():
+    """用戶追蹤名單查詢功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        followed = services.followed_user(data['username'])
+        res.update({'status': True, 'data': followed})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@user.route('/comments')
+@login_required
+def comments():
+    """獲取用戶的評論以及是否按讚功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+
+        if 'username' not in data:
+            res['msg'] = 'Lack of required parameters'
+            return jsonify(res), 401
+
+        username = unquote(data['username'])
+
+        comments = services.get_favorit_comments(username)
+        res.update({'status': True, 'data': comments})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@user.route('/user/visited')
+@login_required
+def visited():
+    """獲取五則用戶最近瀏覽頁面的功能"""
+    res = {'status': False}
+    try:
+        pages = services.visted_pages()
+        res.update({'status':True, 'data':pages})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@user.route('/user/liked')
+@login_required
+def liked():
+    """查詢用戶按讚餐廳狀況"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        username = unquote(data['username'])
+        pages, count = services.query_restaurant(username)
+        res.update({'status':True, 'data': pages, 'count': count})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
--- tripadvisor/api/user/services.py
+++ tripadvisor/api/user/services.py
@@ -4,15 +4,13 @@ from tripadvisor.models import User, TripAdvisor, Comment, Love, Comment_like,Fo
 from tripadvisor import dao, tasks
 
 
-def submit_about_me(username, row):
-    user = User()
-    user = user.find_by_username(username)
-
-    user.city = row.get("city", None)
-    user.website = row.get("website", None)
-    user.about_me = row.get("about_me", None) 
-
-    return user
+def update_aboutMe(username, row):
+    record = tasks.find_user(username)
+    
+    record.city = row.get('city', None)
+    record.website  = row.get('website', None)
+    record.about_me = row.get('about_me', None) 
+    dao.save(record)
 
 
 def submit_comments(id, user, row):
@@ -20,78 +18,26 @@ def submit_comments(id, user, row):
     restaurant = tripadvisor.find_by_id(id)
 
     c = Comment()
-    c.rating = row.get("rating",None)
-    c.friend = row.get("friend",None)
-    c.review_title = row.get("review_title",None)
-    c.review_content = row.get("review_content", None)
-    c.booking_date = row.get("booking_date", None)
-    c.takeout = row.get("takeout",None)
-    c.vegetable = row.get("vegetable",None)
-    c.disabled = row.get("disabled", None)
-    c.star1 = row.get("star1", None)
-    c.star2 = row.get("star2", None)
-    c.star3 = row.get("star3", None)
-    c.recommend_dish = row.get("recommend_dish",None)
+    c.rating = row.get('rating', None)
+    c.friend = row.get('friend', None)
+    c.review_title = row.get('review_title', None)
+    c.review_content = row.get('review_content', None)
+    c.booking_date = row.get('booking_date', None)
+    c.takeout = row.get('takeout', None)
+    c.vegetable = row.get('vegetable', None)
+    c.disabled = row.get('disabled', None)
+    c.star1 = row.get('star1', None)
+    c.star2 = row.get('star2', None)
+    c.star3 = row.get('star3', None)
+    c.recommend_dish = row.get('recommend_dish', None)
     c.author = user._get_current_object()
     c.restaurant = restaurant
-    
-    return c
+
+    dao.save(c)
 
 
-def submit_comment_like(id, user):
-    comment = Comment()
-    comment = comment.find_by_id(id)
-
-    c = Comment_like(user_id = user.id, comment_id = comment.id)
-    return c
-
-
-def submit_comment_unlike(id, current_user):
-    comment = Comment()
-    comment = comment.find_by_id(id)
-    tasks.cancel_follow_comments(current_user.id, comment)
-    
-
-def submit_like(user, restaurant, row):
-    focus = row.get("like", None)
-    tripAdvisor = TripAdvisor()
-    restaurant = tripAdvisor.find_by_name(restaurant)
-    love = Love(focus=focus, store=restaurant, author=user._get_current_object())
-    return love
-
-
-def submit_unlike(current_user, restaurant):
-    tripAdvisor = TripAdvisor()
-    restaurant = tripAdvisor.find_by_name(restaurant)
-    tasks.cancel_follow_restaurant(current_user.id, restaurant)
-
-
-def follow(username, current_user):
-    user = User()
-    user = user.find_by_username(username)
-    if user is None:
-        return {"status":"error","errmsg":"查無該用戶"}
-
-    if current_user.is_following(user):
-        return {"status":"error","errmsg":"已關注該用戶"}
-    
-    else:
-        current_user.follow(user)
-        return {"status":"success", "msg":u"您已成功關注%s"%username}
-
-
-def unfollow(username, current_user):
-    user = User()
-    user = user.find_by_username(username)
-    if user is None:
-        return {"status":"error","errmsg":"查無該用戶"}
-    else:
-        current_user.unfollow(user)
-        return {"status":"success","msg":u"您已成功取消對%s的關注"%username}
-
-
-def following_user(username):
-    followers = tasks.find_followers(username)
+def get_followers(username):
+    followers    = tasks.find_followers(username)
     my_followers = tasks.find_current_followers(current_user)
 
     followers_list = []
@@ -101,17 +47,18 @@ def following_user(username):
     result = []
     for f in followers:
         row = {}
-        row["city"] = f.follower.city
-        row["name"] = f.follower.username
-        row["about_me"] = f.follower.about_me
-        row["follower_count"] = f.follower.followers.count()
+        row['city'] = f.follower.city
+        row['name'] = f.follower.username
+        row['about_me'] = f.follower.about_me
+        row['follower_count'] = f.follower.followers.count()
+
         if f.follower.id != current_user.id:
             if f.follower.id in followers_list:
-                row["follow_status"] = "關注中"
+                row['follow_status'] = '關注中'
             else:
-                row["follow_status"] = "追蹤"
+                row['follow_status'] = '追蹤'
         else:
-            row["follow_status"] = "自己"
+            row['follow_status'] = '自己'
 
         result.append(row)
     
@@ -122,26 +69,71 @@ def followed_user(username):
     followed = tasks.find_followed(username)
     my_followerd = tasks.find_current_followerd(current_user)
     
-    followerd_list=[]
+    followerd_list = []
     for f in my_followerd:
         followerd_list.append(f.followed.id)
     
     result = []
     for f in followed:
         row = {}
-        row["city"] = f.followed.city
-        row["name"] = f.followed.username
-        row["about_me"] = f.followed.about_me
-        row["follower_count"] = f.followed.followers.count()
+        row['city'] = f.followed.city
+        row['name'] = f.followed.username
+        row['about_me'] = f.followed.about_me
+        row['follower_count'] = f.followed.followers.count()
 
         if f.followed.id != current_user.id:
             if f.followed.id in followerd_list:
-                row["follow_status"] = "關注中"
+                row['follow_status'] = '關注中'
             else:
-                row["follow_status"] = "追蹤"
+                row['follow_status'] = '追蹤'
         else:
-            row["follow_status"] = "自己"
-        
+            row['follow_status'] = '自己'
         result.append(row)
     
     return result
+
+
+def get_favorit_comments(username):
+    comment = tasks.author_following_comments(username)
+    my_following_comments = tasks.my_following_comments(current_user.id)
+                            
+    comment_like = [u.id for u in my_following_comments]
+
+    followed_id = [ i.followed_id for i in current_user.followed.all()]
+
+    comments = []
+    for c in comment:
+        row = {}
+        row['follow_condition'] = '關注中' if c.author_id in followed_id else '追蹤' if c.author_id!=current_user.id else "自己"
+        row['like_condition'] = '已讚' if c.id in comment_like else '讚'
+        row['review_content'] = c.review_content
+        row['rating'] = c.rating
+        row['author'] = c.author.username
+        row['title'] = c.restaurant.title
+
+        comments.append(row)
+
+    return comments
+
+
+def visted_pages():
+    records = tasks.get_recent_read(current_user.id)
+    pages = []
+    for record in records:
+        row = {}
+        row = record.to_dict()
+        row['image'] = row['info_url'][3]
+        pages.append(row)
+    
+    return pages
+
+
+def query_restaurant(username):
+    user = User()
+    user = user.find_by_username(username)
+
+    records, count = tasks.following_restaurant(user.id)
+    
+    restaurants = [ record.to_dict() for record in records ]
+
+    return restaurants, count
