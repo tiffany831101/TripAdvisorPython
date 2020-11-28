ac8891139693428a56146cb4589fc73537413983
LinYuMingTaipei <a828215362@gmail.com>
2020-11-28 16:37:15 +0800

    refactor restaurant api

--- tripadvisor/api/restaurant/endpoints.py
+++ tripadvisor/api/restaurant/endpoints.py
@@ -3,92 +3,189 @@ from flask_login import login_required, current_user
 
 from datetime import datetime
 import logging
-import json
+from urllib.parse import unquote 
 
 from tripadvisor import dao, tasks
 from tripadvisor.api.restaurant import services, main
 from tripadvisor.models import User
 
-
 logger = logging.getLogger()
-
-@main.route('/restaurant')
-def index():
-    logger.error("error msg")
-    logger.warn("warn msg")
-    logger.info("info msg")
-    logger.debug("debug msg")
-    return render_template('index.html',current_time=datetime.utcnow())
 
 
 @main.route('/restaurant/list')
 def restaurant_list():
-    return render_template("restaurant.html")
+    return render_template('restaurant.html')
 
 
 @main.route('/comment/<restaurant>')
 def restaurant(restaurant):
-    restaurant, comments = services.get_restaurant_comments(restaurant)
-    return render_template("review.html",restaurant=restaurant, comment=comments)
+    return render_template('review.html')
 
 
-@main.route('/restaurant/<restaurant>',methods=["POST","GET"])
+@main.route('/restaurant/<restaurant>',methods=['POST', 'GET'])
 @login_required
 def search_result(restaurant):
     restaurant, comments, like, love =  services.get_restaurant_pages(restaurant)
-    
     return render_template("res_result.html", data=restaurant, like=like,\
-                                                love=love, comment=comments) 
-
+                                                love=love, comment=comments)                                            
 
 @main.route('/user/<username>')
 @login_required
 def user(username):
-    read = services.recent_read_page()
     comments = services.get_favorit_comments(username)
-    user, restaurants, count = services.get_favorit_restaurant(username)
 
-    return render_template('user.html',store_list=restaurants, count=count, comment_list=comments, user=user, read_list=read)
+    user = User().find_by_username(username)
+
+    return render_template('user.html', comment_list=comments, user=user)
 
 
-@main.route("/restaurant/filter")
+@main.route('/restaurant/filter')
 def restaurant_filter():
-    page = request.args.get("page")
-    page = int(page) if page else 1
-    
-    redis_key, result = services.get_restaurant_pages_by_page(page)
+    """獲取餐廳的列表信息(搜索頁面)"""
+    res = {'status': False}
+    try:
+        page = request.args.get('page')
+        page = int(page) if page else 1
 
-    if result:
-        return result, 200, {"Content-Type":"application/json"}
-
-    result = services.get_restaurant_info_from_mysql_by_page(page)
-    result = json.dumps(result)
-    services.cache(redis_key,page, result)
+        result_dict = services.fuzzy_search(page)
+        
+        res.update({'status': True})
+        res.update(result_dict)
+        return res, 200, {'Content-Type': 'application/json'}
     
-    return result, 200, {"Content-Type":"application/json"}
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
     
 
 @main.route('/restaurant/search')
 def restaurant_search():
-    """獲取餐廳的列表信息(搜索頁面)"""
-    data = request.args.to_dict()
-    data["page"] = int(data["page"]) if 'page' in data else 1
-    data["user"] = current_user.get_id()
+    """根據搜尋條件，獲取餐廳的列表信息"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        data['page'] = int(data['page']) if 'page' in data else 1
+        data['user'] = current_user.get_id()
+        
+        query = services.query(**data)
+        result_dict = services.to_dict(data, query)
 
-    redis_key, result = services.get_restaurant_info_from_redis(**data)
-
-    if result:
-        return result, 200, {"Content-Type":"application/json"}
+        res.update({'status': True})
+        res.update(result_dict)
+        return res, 200, {'Content-Type':'application/json'}
     
-    restaurant = services.get_restaurant_info_from_mysql(**data)
-    result = services.get_result_list(data, restaurant)
-    services.cache(redis_key, data["page"], json.dumps(result))
-    return result, 200, {"Content-Type":"application/json"}
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@main.route('/restaurant/city')
+def get_city():
+    """獲取餐廳城市功能"""
+    res = {'status': False}
+    try:
+        cities = tasks.get_city()
+
+        res.update({'status':True, 'data': cities})
+
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@main.route('/restaurant/area')
+def get_area():
+    """獲取餐廳地區功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        
+        if 'city' not in data:
+            res['msg'] = 'Lack of required parameters'
+            return jsonify(res), 401
+        
+        data = tasks.get_area(data['city'])
+        areas = []
+        for area in data:
+            areas.append(area)
+        
+        res.update({'status':True, 'data': areas})
+
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@main.route('/restaurant/hito')
+def get_top10_restaurant():
+    """獲取前十名餐廳功能"""
+    res = {'status': False}
+    try:
+        data = tasks.get_top10_restaurant()
+        restaurants = []
+        for restaurant in data:
+            restaurants.append(restaurant)
+        res.update({'status': True, 'data': restaurants})
+        return jsonify(res)
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
 
 @main.route('/read/count/<restaurant>')
 def read_count(restaurant):
-    submit = services.submit_click(restaurant, current_user)
-    result = dao.save(submit)
-    return jsonify(result)
+    res = {'status': False}
+    try:
+        submit = services.submit_click(restaurant, current_user)
+        dao.save(submit)
+        res.update({'status': True})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
 
+
+@main.route('/restaurant/comments')
+def get_comments():
+    """獲取餐廳評論功能"""
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+
+        if 'restaurant' not in data:
+            res['msg'] = 'Lack of required parameters'
+            return jsonify(res), 401
+
+        restaurant = unquote(data['restaurant'])
+        comments = tasks.get_comments(restaurant)
+        res.update({'status': True, 'data': comments})
+        return jsonify(res)
+    
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
+
+
+@main.route('/restaurant/review')
+def restaurant_review():
+    res = {'status': False}
+    try:
+        data = request.args.to_dict()
+        if 'restaurant' not in data:
+            res['msg'] = 'Lack of required parameters'
+            return jsonify(res), 401
+            
+        restaurant = unquote(data['restaurant'])
+        information, reviews = services.get_restaurant_comments(restaurant)
+        res.update({'status': True, 'information': information, 'reviews': reviews})
+        return jsonify(res)
+
+    except Exception as e:
+        logger.error(e)
+        return jsonify(res)
--- tripadvisor/api/restaurant/services.py
+++ tripadvisor/api/restaurant/services.py
@@ -3,37 +3,18 @@ from flask_login import login_required, current_user
 import logging
 
 from tripadvisor.models import User, TripAdvisor, Comment,\
-                                Love, Comment_like, Click
-from tripadvisor import redis_store, tasks
+                                Comment_like, Click
+from tripadvisor import tasks, cache
 from tripadvisor.settings.defaults import *
 
 logger = logging.getLogger()
 
-
-def get_redis_key(address, res_type, sort_key, page, keyword, user):
-    return f"restaurant_{address}_{res_type}_{sort_key}_{page}_{keyword}_{user}" 
+CACHE_TIMEOUT = 60 * 10
 
 
-def get_restaurant_pages_by_page(page):
-    redis_key = f"restaurant_{page}"
-    try:
-        result = redis_store.hget(redis_key, page)
-    except Exception as e:
-        current_app.logger.error(e)
-    return redis_key, result
-
-
-def get_favorit_restaurant(username):
-    if username is not current_user.username:
-        user = User().find_by_username(username)
-        if not user:
-            return {"status":"error", "msg":"查無該用戶"}
-        
-    restaurant, count = tasks.following_restaurant(user.id)
-    
-    restaurants = [ r.to_dict() for r in restaurant ]
-
-    return user, restaurants, count
+def get_userId():
+    user_id = current_user.get_id()
+    return current_user.id if user_id else 0
 
 
 def get_favorit_comments(username):
@@ -47,126 +28,111 @@ def get_favorit_comments(username):
     comments = []
     for c in comment:
         row = {}
-        row["follow_condition"] = "關注中" if c.author_id in followed_id else "追蹤" if c.author_id!=current_user.id else "自己"
-        row["like_condition"] = "已讚" if c.id in comment_like else "讚"
-        row["review_content"] = c.review_content
-        row["rating"] = c.rating
-        row["author"] = c.author.username
-        row["title"] = c.restaurant.title
+        row['follow_condition'] = '關注中' if c.author_id in followed_id else '追蹤' if c.author_id!=current_user.id else "自己"
+        row['like_condition'] = '已讚' if c.id in comment_like else '讚'
+        row['review_content'] = c.review_content
+        row['rating'] = c.rating
+        row['author'] = c.author.username
+        row['title'] = c.restaurant.title
 
         comments.append(row)
 
     return comments
 
 
-def recent_read_page():
-    user_id = current_user.id
-    recent_read = tasks.get_recent_read(user_id)
-    read = []
-    for r in recent_read:
-        row = {}
-        row = r.to_dict()
-        row["image"] = row["info_url"][3]
-        read.append(row)
-    
-    return read
+def query(**kwargs):
+    user_id = get_userId()
 
-
-def get_restaurant_info_from_redis(**kwargs):
-    redis_key = get_redis_key(kwargs["area"], kwargs["food"], kwargs["filter"], \
-                                kwargs["page"], kwargs["keyword"], kwargs["user"])
-    try:
-        result = redis_store.hget(redis_key, kwargs["page"])
-    except Exception as e:
-        logger.error(e)
-    return redis_key, result
-
-
-def get_restaurant_info_from_mysql(**kwargs):
-    if not kwargs["user"]:
-        user_id = 1
-    else:
-        user_id = current_user.id
-    
     filter_params = []
-    if kwargs["keyword"]:
-        filter_params.append(TripAdvisor.title.contains(kwargs["keyword"]))
+    if kwargs['keyword']:
+        filter_params.append(TripAdvisor.title.contains(kwargs['keyword']))
 
-    if kwargs["area"]:
-        filter_params.append(TripAdvisor.address==kwargs["area"])  
+    if kwargs['area']:
+        filter_params.append(TripAdvisor.area == kwargs['area'])  
 
-    if kwargs["food"]:
-        filter_params.append(TripAdvisor.res_type.contains(kwargs["food"]))    
+    if kwargs['food']:
+        filter_params.append(TripAdvisor.res_type.contains(kwargs['food']))    
 
-    if kwargs["filter"] == "評論數":
+    if kwargs['filter'] == '評論數':
         restaurant = tasks.comment_search(int(user_id), params=filter_params)
 
-    elif kwargs["filter"] == "評分數":
+    elif kwargs['filter'] == '評分數':
         restaurant = tasks.rating_search(int(user_id), params=filter_params)
-        
+    
     return restaurant
 
 
-def to_result_dict(restaurant_object):
-    items = restaurant_object.items
-    pages = restaurant_object.pages
-    if not items:
-        return {"status":"error", "errmsg":"參數錯誤"}
-
+def get_result(restaurant_object):
+    items  = restaurant_object.items
+    pages  = restaurant_object.pages
     result = []
-    for i in items:
+
+    if not items:
+        return
+
+    for item in items:
         row = {}
-        row = i[0].to_dict()
-        row["info_url"] = row["info_url"][0]
-        row["comment"] = row["comment"][0]
-        row["love"] = "已收藏"
-        if current_user.get_id() and i[1]==current_user.username:
-            row["love"] = "已收藏"
+        row = item[0].to_dict()
+        row['info_url'] = row['info_url'][0]
+        row['comment']  = row['comment'][0]
+        row['love'] = '已收藏'
+        
+        if current_user.get_id() and item[1] == current_user.username:
+            row['love'] = '已收藏'
         else:
-            row["love"] = "收藏"
+            row['love'] = '收藏'
     
         result.append(row)
 
     return pages, result
 
 
-def get_result_list(data, restaurant):
-    restaurant_object = restaurant.paginate(page=data["page"], per_page=30, error_out=False)
-    pages, result = to_result_dict(restaurant_object)
-    result_dict = dict(errno=1,data=result, total_page=pages, params=data)
+@cache.memoize(CACHE_TIMEOUT)
+def to_dict(data, restaurant):
+    restaurant_object = restaurant.paginate(page = data['page'], per_page = 30, error_out = False)
+    pages, result = get_result(restaurant_object)
 
-    return result_dict
+    if result:
+        result_dict = dict(data = result, total_page = pages, params = data)
+        return result_dict
 
 
-def get_restaurant_info_from_mysql_by_page(page):
-    user = current_user.get_id()
-    user_id = current_user.id if user else 0
+@cache.memoize(CACHE_TIMEOUT)
+def fuzzy_search(page):
+    user_id = get_userId()
+    
     restaurant_object = tasks.page_filter(user_id, page=page)
-
-    pages, result = to_result_dict(restaurant_object)
-    result_dict = dict(errno=1, data=result, total_page=pages)
+    pages, result = get_result(restaurant_object)
+    
+    if not result:
+        return
+    
+    result_dict = dict(data=result, total_page=pages)
     return result_dict
 
 
 def get_restaurant_comments(name):
     restaurant = TripAdvisor().find_by_name(name)
 
-    brief_info = restaurant.to_comment_dict()
+    if not restaurant:
+        return
     
-    comments = Comment().get_comments(restaurant.id)
+    information = restaurant.to_comment_dict()
 
-    restaurant_comments = [ c.to_dict() for c in comments ]
+    records = Comment().get_comments(restaurant.id)
     
-    return brief_info, restaurant_comments
+    reviews = [ record.to_dict() for record in records ]
+    
+    return information, reviews
 
 
 def submit_click(restaurant, current_user):
     store = TripAdvisor().find_by_name(restaurant)
     if current_user.is_authenticated:
-        c = Click()
-        c.store_id = store.id
-        c.user_id = current_user.id
-
+        c = Click(
+            store_id = store.id,
+            user_id = current_user.id
+        )
         store.read_count = store.read_count + 1 
         return c
 
@@ -177,19 +143,8 @@ def get_restaurant_pages(name):
     comments = Comment().find_all(restaurant.id)
 
     likes = Comment_like().find_all(current_user.id)
-
-    love = Love().find_by_store_and_user(restaurant.id, current_user.id)
+    
+    love = tasks.like_restaurant(restaurant.id, current_user.id)
 
     restaurant = restaurant.to_dict()
     return restaurant, comments, likes, love
-
-
-def cache(redis_key, page, data):
-    try:
-        pipeline = redis_store.pipeline()
-        pipeline.multi()
-        pipeline.hset(redis_key, page, data)
-        pipeline.expire(redis_key, RESTAURANT_LIST_PAGE)
-        pipeline.execute()
-    except Exception as e:
-        current_app.logger.error(e)
