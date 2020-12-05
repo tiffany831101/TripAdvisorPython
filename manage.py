15185103454bcf8202b77580222048b21b9791f8
LinYuMingTaipei <a828215362@gmail.com>
2020-12-05 12:54:08 +0800

    add uwsgi monitor and its configurations

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 import tripadvisor
 import flask_script as script
 from flask_migrate import Migrate, MigrateCommand
+from prometheus_flask_exporter.multiprocess import UWsgiPrometheusMetrics
 from tripadvisor.models import User, Follow, TripAdvisor, Comment,\
                              Love, Child_cmt, Click, Comment_like
 
@@ -13,11 +14,17 @@ manager.add_command('shell', script.Shell(make_context=lambda: {
     'current_app': app
 }))
 
+try:
+    metrics = UWsgiPrometheusMetrics(app=app, default_labels={'project': 'hotel'})
+    metrics.start_http_server(9200)
+except ModuleNotFoundError:
+    pass
+
 
 def make_shell_context():
     return dict(app=app, db=tripadvisor.db, User=User, Follow=Follow, Food=Food, Survey=Survey, TripAdvisor=TripAdvisor)
 
-manager.add_command('db',MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 if __name__=="__main__":
     manager.run()
